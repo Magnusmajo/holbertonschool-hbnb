7e38ec65b5a0dc023eb0d70ef2fd56e0750e6ffa
Magnusmajo <alexisoblivion@gmail.com>
2024-11-07 17:01:31 -0300

    Classess

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,12 +1,17 @@
 from app.models.base import BaseModel
+from datetime import datetime
 
 class User(BaseModel):
+    """A class to represent a user in the application"""
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
+        self.id = self.generate_id()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.is_admin = is_admin
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __repr__(self):
         return f"<User {self.first_name} {self.last_name}>"
