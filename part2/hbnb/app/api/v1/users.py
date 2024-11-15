5c218834eb6181d380fb3cddd0d8ebd92648b117
Magnusmajo <alexisoblivion@gmail.com>
2024-11-15 15:56:32 -0300

    Updating User Endpoint

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -11,6 +11,7 @@ user_model = api.model('User', {
     'email': fields.String(required=True, description='Email of the user')
 })
 
+
 @api.route('/')
 class UserList(Resource):
     @api.expect(user_model, validate=True)
@@ -29,26 +30,32 @@ class UserList(Resource):
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
+    @api.response(200, 'Users retrieved successfully')
+    def get(self):
+        """Retrieve a list of users"""
+        users = facade.get_all_users()  # Obtener todos los usuarios
+        return [{'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email} for user in users], 200
+
 @api.route('/<user_id>')
 class UserResource(Resource):
-    @api.response(200, 'User details retrieved successfully')
-    @api.response(404, 'User not found')
+    @api.response(200, 'User  details retrieved successfully')
+    @api.response(404, 'User  not found')
     def get(self, user_id):
         """Get user details by ID"""
         user = facade.get_user(user_id)
         if not user:
-            return {'error': 'User not found'}, 404
+            return {'error': 'User  not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
 
-    @api.response(200, 'User successfully updated')
-    @api.response(400, 'Invalid input data')
     @api.expect(user_model, validate=True)
+    @api.response(200, 'User  successfully updated')
+    @api.response(404, 'User  not found')
+    @api.response(400, 'Invalid input data')
     def put(self, user_id):
-        """Update user details by ID"""
-        user = facade.get_user(user_id)
-        if not user:
-            return {'error': 'User not found'}, 404
-
+        """Update user information"""
         user_data = api.payload
         updated_user = facade.update_user(user_id, user_data)
+        if not updated_user:
+            return {'error': 'User  not found'}, 404
+        
         return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
