c2d2830d37328a0bdb790ebabd899a1c60c7fb4d
balemansteve <balemansteve@gmail.com>
2024-11-20 03:35:22 -0300

    Change Swagger URL

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -12,7 +12,7 @@ from app.api.v1.reviews import api as reviews_ns
 
 def create_app(config_class='config.DevelopmentConfig'):
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     app.config.from_object(config_class)  # Load the configuration from the specified class
     # bcrypt.init_app(app)
     # jwt.init_app(app)
