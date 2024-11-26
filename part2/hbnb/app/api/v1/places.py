7fc8d2e126694bd9bbc6067efd4fb54b352446df
Magnusmajo <alexisoblivion@gmail.com>
2024-11-26 12:12:54 -0300

    hbnb

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -87,21 +87,21 @@ class PlaceResource(Resource):
                 return {'error': 'Place not found'}, 404
             
             return {
-                'id': place.id if place.id else None,
-                'title': place.title if place.title else None,
-                'description': place.description if place.description else None,
-                'latitude': place.latitude if place.latitude else None,
-                'longitude': place.longitude if place.longitude else None,
-                'owner': {
-                    'id': place.owner.id if place.owner and place.owner.id else None,
-                    'first_name': place.owner.first_name if place.owner and place.owner.first_name else None,
-                    'last_name': place.owner.last_name if place.owner and place.owner.last_name else None,
-                    'email': place.owner.email if place.owner and place.owner.email else None
-                } if place.owner else None,
-                'amenities': [{
-                    'id': amenity.id,
-                    'name': amenity.name
-                } for amenity in place.amenities] if place.amenities else []
+            'id': place.id,
+            'title': place.title,
+            'description': place.description,
+            'latitude': place.latitude,
+            'longitude': place.longitude,
+            'owner': {
+                'id': place.owner.id,
+                'first_name': place.owner.first_name,
+                'last_name': place.owner.last_name,
+                'email': place.owner.email
+            } if place.owner else None,
+            'amenities': [{
+                'id': amenity.id,
+                'name': amenity.name
+            } for amenity in place.amenities] if place.amenities else []
             }, 200
         except Exception as e:
             return {'error': str(e)}, 500
