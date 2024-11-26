e09fba697992aa23eb81edc5ad9b0c5e20bafa9e
Magnusmajo <alexisoblivion@gmail.com>
2024-11-26 00:09:12 -0300

    HBnB

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns
+# from app.api.v1.users import api as users_ns
+# from app.api.v1.amenities import api as amenities_ns
+# from app.api.v1.places import api as places_ns
+# from app.api.v1.reviews import api as reviews_ns
+# from app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
@@ -23,13 +23,13 @@ def create_app(config_class='config.DevelopmentConfig'):
     jwt.init_app(app)
     db.init_app(app)
 
-    # Register the users namespace
-    api.add_namespace(users_ns, path='/api/v1/users')
-    # Register the amenities namespace
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    # Register the places namespace
-    api.add_namespace(places_ns, path='/api/v1/places')
-    # Register the reviews namespace
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(auth_ns, path='/api/v1/auth')
+    # # Register the users namespace
+    # api.add_namespace(users_ns, path='/api/v1/users')
+    # # Register the amenities namespace
+    # api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    # # Register the places namespace
+    # api.add_namespace(places_ns, path='/api/v1/places')
+    # # Register the reviews namespace
+    # api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    # api.add_namespace(auth_ns, path='/api/v1/auth')
     return app
