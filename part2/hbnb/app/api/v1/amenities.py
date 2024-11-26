3b711f9ed498e6ebc8a493d03f6ab5d12c3f628f
Magnusmajo <alexisoblivion@gmail.com>
2024-11-26 10:47:18 -0300

    HBnB

--- part2/hbnb/app/api/v1/amenities.py
+++ part2/hbnb/app/api/v1/amenities.py
@@ -17,7 +17,7 @@ class AmenityList(Resource):
         """Register a new amenity"""
         data = api.payload
         try:
-            amenity = facade.create_amenity(data)  # Calling the facade method
+            amenity = facade.create_amenity(data)  # llama a la fachada para crear una amenity
             return {
                 'id': amenity.id,
                 'name': amenity.name
@@ -29,13 +29,13 @@ class AmenityList(Resource):
     def get(self):
         """Retrieve a list of all amenities"""
         # Placeholder for logic to return a list of all amenities
-        amenities = facade.get_all_amenities()  # Call the facade method
+        amenities = facade.get_all_amenities()  # llama a la fachada para obtener las amenities
         return [
             {
                 'id': amenity.id,
                 'name': amenity.name
                 } for amenity in amenities
-                ], 200  # Return the list of amenities
+                ], 200  # devuelve la lista de amenities
 
 
 @api.route('/<amenity_id>')
@@ -62,6 +62,5 @@ class AmenityResource(Resource):
         amenity = facade.update_amenity(amenity_id, data)
 
         if amenity:
-            return {'message': 'Amenity updated successfully'}, 200  # Return success message
-        return {'error': 'Amenity not found'}, 404  # Handle not found
-    
+            return {'message': 'Amenity updated successfully'}, 200
+        return {'error': 'Amenity not found'}, 404
