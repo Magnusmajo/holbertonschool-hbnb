1b8bd509c6992dd0c674e42d8e3156809730876c
Magnusmajo <alexisoblivion@gmail.com>
2024-11-26 10:30:44 -0300

    HbNb

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,5 +1,7 @@
 from app.services.repositories.user_repository import UserRepository
 from app.models.user import User
+from app.models.place import Place
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -16,3 +18,39 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_user_by_email(email)
+    
+    def create_place(self, place_data):
+        place = Place(**place_data)
+        self.place_repo.add(place)
+        return place
+    
+    def get_place(self, place_id):
+        return self.place_repo.get(place_id)
+    
+    def get_places(self):
+        return self.place_repo.get_all()
+    
+    def get_places_by_user(self, user_id):
+        return self.place_repo.get_places_by_user(user_id)
+    
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+    
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+    
+    def get_amenities(self):
+        return self.amenity_repo.get_all()
+    
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+    
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+    
+    def get_reviews(self):
+        return self.review_repo.get_all()
